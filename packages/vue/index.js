efda2040f16e6ca7ec519ab5e89a6774f84f378b
yanquanfahei <1449826851@qq.com>
2023-02-12 19:41:38 +0800

    feat: vue eslint

---
+++ packages/vue/index.js
@@ -0,0 +1,28 @@
+const { isPackageExists } = require('local-pkg')
+
+const TS = isPackageExists('typescript')
+
+module.exports = {
+  overrides: [
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        parserOptions: {
+          project: ['./tsconfig.json'],
+          tsconfigRootDir: process.cwd(),
+        }
+      },
+    }
+  ],
+  extends: [
+    'plugin:vue/vue3-recommended',
+    TS
+      ? '@eye-socket/eslint-config-ts'
+      : '@eye-socket/eslint-config-basic'
+  ],
+  rules: {
+    'vue/multi-word-component-names': 'off'
+  }
+}
---
+++ packages/vue/readme.md
@@ -0,0 +1,17 @@
+# @eye-socket/eslint-config-vue
+
+## Install
+
+```shell
+pnpm add -D @eye-socket/eslint-config-vue
+```
+
+## Usage
+
+```json
+// .eslintrc
+
+{
+  "extends": "@eye-socket/eslint-config-vue"
+}
+```
